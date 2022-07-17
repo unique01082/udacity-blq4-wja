9b4c10bbe95d323337783a01d906e8484fd7a2b5
Bao Le <>
2022-07-17 19:01:40 +0700

    feat: implement weather journal app

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
+// Require Express to run server and routes
+const express = require("express");
+// Require body-parser to parse request body
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 // Setup empty JS object to act as endpoint for all routes
 projectData = {};
 
-// Require Express to run server and routes
-
 // Start up an instance of app
+const app = express();
+const port = 3000;
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
-app.use(express.static('website'));
-
+app.use(express.static("website"));
 
 // Setup Server
+app.get("/all", (req, res) => {
+  res.send(projectData);
+});
+
+app.post("/submit", async (req, res) => {
+  projectData = req.body;
+  res.send(projectData);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
--- website/app.js
+++ website/app.js
@@ -1,5 +1,108 @@
 /* Global Variables */
+const apiKey = "923fe800aca046b69819c7c974ad2e28";
+const button = document.getElementById("generate");
+const zip = document.getElementById("zip");
+const feelings = document.getElementById("feelings");
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+/**
+ * Handle submit event
+ */
+const handleSubmit = async () => {
+  try {
+    // Get zip value
+    const zipValue = zip.value;
+
+    // Get weather from openweathermap
+    const data = await fetchData({ zip: zipValue });
+
+    // Get feelings value
+    const feelingsValue = feelings.value;
+
+    // Send data to server
+    await sendData({
+      zip: zipValue,
+      feel: feelingsValue,
+      date: newDate,
+      temp: data.main.temp,
+    });
+
+    // Fetch newest data and re-render UI
+    await retrieveData();
+  } catch (error) {
+    alert(error.message);
+  }
+};
+
+/**
+ * Send data to server
+ * @param {object} payload
+ * @returns
+ */
+const sendData = (payload) =>
+  fetch("/submit", {
+    method: "POST",
+    body: JSON.stringify(payload),
+    headers: { "Content-Type": "application/json" },
+  });
+
+/**
+ * Get most recent entry from server
+ */
+const retrieveData = async () => {
+  const request = await fetch("/all");
+  // Transform into JSON
+  const allData = await request.json();
+  // Write updated data to DOM elements
+  updateUI(allData);
+};
+
+/**
+ * Update UI base on data
+ * @param {object} data
+ */
+const updateUI = (data) => {
+  if (Object.keys(data).length) {
+    document.getElementById("temp").innerHTML =
+      Math.round(data.temp) + " degrees";
+    document.getElementById("content").innerHTML = data.feel;
+    document.getElementById("date").innerHTML = data.date;
+    document.getElementById("zipcode").innerHTML = data.zip;
+  }
+};
+
+/**
+ *
+ * @param {*} params
+ * @returns
+ */
+const fetchData = async (params = {}) => {
+  // Build endpoint string with parameters
+  let url = new URL("https://api.openweathermap.org/data/2.5/weather");
+  url.searchParams.append("units", "imperial");
+  url.searchParams.append("appId", apiKey);
+  Object.keys(params).forEach((key) =>
+    url.searchParams.append(key, params[key])
+  );
+
+  // Send request
+  const response = await fetch(url);
+  // Parse response
+  const data = await response.json();
+
+  // Check if response succeeded
+  if (data?.cod !== 200) {
+    throw new Error(data?.message);
+  }
+
+  return data;
+};
+
+// Fetch most recent entry from server on
+window.addEventListener("load", retrieveData);
+
+// Add event listener for button
+button.addEventListener("click", handleSubmit);
